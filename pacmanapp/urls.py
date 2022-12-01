bbf38d63297e107e7193a047e034c2f467db8955
Kevin Lange <kevinlange97@icloud.com>
2022-12-01 10:14:31 +0100

    [ENH15] - Add Basic structure to the project
    Commit adds the basic structure of the three different pages to the repository.

---
+++ pacmanapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('pacman', views.pacman, name='pacman'),
+    path('profile', views.profile, name='profile')
+]
--- pacmanapp/views.py
+++ pacmanapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """
+    Returns the index.html for testing purposes
+    """
+    return render(request, 'index.html', {})
+
+
+def pacman(request):
+    """
+    Returns the pacman.html
+    """
+    return render(request, 'pacman.html', {})
+
+def profile(requeest):
+    """
+    Returns the profile.html
+    """
+    return render(requeest, 'profile.html', {})
