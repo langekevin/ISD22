3f33e4a5b836e5f9cc34c3d8dc90dc54fa05c307
gianlucafr <gianluca.frisch@uni.li>
2022-12-23 16:08:29 +0100

    Login process
    Login process with django authentification form

--- pacmanapp/urls.py
+++ pacmanapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
     path('pacman', views.pacman, name='pacman'),
     path('profile', views.profile, name='profile'),
     path('registration', views.registration, name='registration')
--- pacmanapp/views.py
+++ pacmanapp/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
 
 def index(request):
     """
     Returns the index.html for testing purposes
     """
-    return render(request, 'index.html', {})
+    form = AuthenticationForm()
+    return render(request, 'index.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('pacman')
+            else:
+                print("user is not available")
+        else:
+            print("form is not valid")
+    return redirect('index')
 
 
 def pacman(request):
     """
     Returns the pacman.html
     """
+    if request.user.is_authenticated:
+        username = request.user.username
+        pk = request.user.pk
+        print(pk)
+        print(username)
     return render(request, 'pacman.html', {})
 
 def profile(requeest):
